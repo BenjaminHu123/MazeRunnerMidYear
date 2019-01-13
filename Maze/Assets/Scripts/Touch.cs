7db570e1be0fbc7f673f65cda0d20a9fffa6aede
BenjaminHu123 <43998171+BenjaminHu123@users.noreply.github.com>
2019-01-13 09:35:15 -0500

    Project

---
+++ Maze/Assets/Scripts/Touch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Touch : MonoBehaviour {
+    private ScoreTracker score;
+    public AudioSource audio;
+    private Transform t;
+    private Transform cam;
+    private void Start()
+    {
+        score = GetComponent<ScoreTracker>();
+        t = GameObject.Find("FPSController").transform;
+        cam = GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene("Maze");
+        }
+        cam = t;
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("coin"))
+        {
+            audio.Play();
+            collision.transform.gameObject.SetActive(false);
+            score.addScore();
+        }
+    }
+}
