51ed64aef882d75d14d05f968a49f12f447d639a
charlesyu1203 <43996689+charlesyu1203@users.noreply.github.com>
2019-01-16 23:05:03 -0500

    Final change pt2

--- Maze/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Maze/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -46,6 +46,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private float m_NextStep;
         private bool m_Jumping;
         private AudioSource m_AudioSource;
+      
 
         // Use this for initialization
         private void Start()
